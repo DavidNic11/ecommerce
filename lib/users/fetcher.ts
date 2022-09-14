ef7a2d667ab34be64503e8b7c94a02db1be99a42
Dylan Rogers <dr233615@ohio.edu>
2022-09-14 13:43:15 -0400

    Added fetch calls and logic

--- lib/users/fetcher.ts
+++ lib/users/fetcher.ts
@@ -1,25 +1,5 @@
-const tempUser = {
-  id: 1,
-  email: "John@gmail.com",
-  username: "johnd",
-  password: "m38rmF$",
-  name: {
-    firstname: "John",
-    lastname: "Doe",
-  },
-  address: {
-    city: "kilcoole",
-    street: "7835 new road",
-    number: 3,
-    zipcode: "12926-3874",
-    geolocation: {
-      lat: "-37.3159",
-      long: "81.1496",
-    },
-  },
-  phone: "1-570-236-7033",
-};
+import { User } from "./types";
 
-export function getUser() {
-  return tempUser;
+export function getUser(): Promise<User> {
+  return fetch("https://fakestoreapi.com/users/1").then((res) => res.json());
 }
--- scenes/User/User.tsx
+++ scenes/User/User.tsx
@@ -1,10 +1,43 @@
 import Image from "next/image";
 import styles from "./User.module.css";
-import { getUser, formatAddress } from "@lib/users";
+import { getUser } from "@lib/users";
 import UserInformation from "./components/UserInformation";
+import { useEffect, useState } from "react";
+import type { User as UserType } from "@lib/users";
+
+const useUser = () => {
+  const [user, setUser] = useState<UserType>();
+  const [fetchError, setFetchError] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    getUser()
+      .then((res) => {
+        if (isMounted) setUser(res);
+      })
+      .catch(() => {
+        setFetchError(true);
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return { isError: fetchError, user };
+};
 
 const User = () => {
-  const user = getUser();
+  const { isError, user } = useUser();
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching user</div>;
+  }
 
   return (
     <div className={styles.container}>
